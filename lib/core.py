e3ddce12aef0c6909cf6a5eb58ca69bcdf4b4a87
liuxuefeng1997 <m15847194612@163.com>
2026-09-16 11:28:00 +0800

    优化日志配置

--- lib/core.py
+++ lib/core.py
@@ -25,7 +25,7 @@ config = Config(config_path)
 
 def init_log():
     NOW_TIME_WITH_NO_SPACE = time.strftime('%Y%m%d_%H%M%S', time.localtime(time.time()))
-    config.write("gui.json", "log", "level_help", "10 = DEBUG, 20 = INFO, 30 = WARNING, 40 = ERROR, 50 = CRITICAL, 你可以在此分区添加level节点来设置日志级别，默认级别为 INFO")
+    config.write("gui.json", "log", "level_help", "10 = DEBUG, 20 = INFO, 30 = WARNING, 40 = ERROR, 50 = CRITICAL, 支持以上级别，默认为 INFO")
     logLevel = config.read("gui.json", "log", "level", logging.INFO)
     if getattr(sys, 'frozen', False):
         if not os.path.exists(log_path):
